15c17a1d1dabaf5f9a6b94076f95278717865fa7
unknown <berrut.webdev@gmail.com>
2019-09-19 09:08:41 +0200

    Exercices finis

--- 13.1- Exercice - Fonctions/fonction.py
+++ 13.1- Exercice - Fonctions/fonction.py
@@ -4,3 +4,13 @@ texte = "ceci est un texte que vous pouvez modifier mais gare aux caracteres spe
 tab_lettres = [
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
      'x', 'y', 'z', ' '], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+def maFonction():
+    for item in enumerate(tab_lettres[0]):
+        try:
+            tab_lettres[1][item[0]] = texte.count(item[1])
+        except:
+            print("Erreur de donnée")
+
+maFonction()
+print(tab_lettres[1])
